d041c6c9b96d27769927edfc8bf6cd5f6a96b9cf
defproger <defproger@gmail.com>
2022-08-11 18:47:01 +0300

    deletepopup

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,9 @@
 function open(element) {
     element.classList.add("active");
 }
+function closepopup(element) {
+    element.classList.remove("active");
+}
 
 // Первый вариант
 
@@ -33,10 +36,26 @@ if (params.soucre === "from_free_veb") {
     setTimeout(() => {
         open(p[1])
     }, 10000);
+
+    //вместо этого онклик
+    setTimeout(() => {
+        closepopup(p[1])
+    }, 12000);
+    /////////////////////
+
+
+
 } else if (getCookie('popap') === undefined) {
     setTimeout(() => {
         open(p[0])
     }, 10000);
+
+    //вместо этого онклик
+    setTimeout(() => {
+        closepopup(p[0])
+    }, 12000);
+    /////////////////////
+
     document.cookie = "popap=true; max-age=43200";
 }
 
