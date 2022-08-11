918b6221d83bb347701e021aa41320396c6ea6e4
defproger <defproger@gmail.com>
2022-08-11 18:08:53 +0300

    popaps

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,43 @@
-const open = element => element.classList.add("active");
+function open(element) {
+    element.classList.add("active");
+}
 
 // Первый вариант
-const p1 = document.getElementById('pp1');
-const p2 = document.getElementById('pp2');
-// open(p1);
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 
 // Второй вариант
 const p = document.getElementsByClassName('popup_div');
-// open(p[1]);
+var params = window
+    .location
+    .search
+    .replace('?', '')
+    .split('&')
+    .reduce(
+        function (p, e) {
+            var a = e.split('=');
+            p[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+            return p;
+        },
+        {}
+    );
+
+
+if (params.soucre === "from_free_veb") {
+    setTimeout(() => {
+        open(p[1])
+    }, 10000);
+} else if (getCookie('popap') === undefined) {
+    setTimeout(() => {
+        open(p[0])
+    }, 10000);
+    document.cookie = "popap=true; max-age=43200";
+}
+
+
